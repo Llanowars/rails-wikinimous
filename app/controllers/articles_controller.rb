79452c1314b93c23ac458e4ea8527bc9f5de9d56
Julien Da Silva <julien.da.silva@hotmail.com>
2017-07-04 11:13:10 -0400

    checkpoint

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,17 +1,36 @@
 class ArticlesController < ApplicationController
 
-def index
-  @articles = Article.all
-end
+  before_action :set_article, only: [ :show, :destroy ]
 
-def show
-  @article = Article.find(params[:id])
-end
+  def index
+    @articles = Article.all
+  end
 
+  def show
+  end
 
+  def new
+    @article = Article.new 
+  end
 
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to article_path(@article)
+  end
 
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
 
+  private 
 
+  def set_article
+    @article = Article.find(params[:id])
+  end
 
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Article.destroy_all
 10.times do
-Article.create(title: Faker::Name.title, content: Faker::Hipster.sentences)
+  Article.create(title: Faker::Name.title, content: Faker::Hipster.sentences)
 end
